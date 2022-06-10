06b7d488ee4126182b0da46d7943b96ebb273a0a
faraigit <faraimax@gmail.com>
2022-06-10 02:17:04 +0200

    Spei_cor_per_quantile_Area

--- scripts/spi_avg_quantiles_province.R
+++ scripts/spi_avg_quantiles_province.R
@@ -1,10 +1,8 @@
-
 library(dplyr)
-library(explore)
 library(readr)
 
-p_mean <- read.csv("SPI_forecast_results_all/Timeseries.FBI.Payout.mean.csv")
-province_district <- read.csv("SPI_forecast_results_all/province district.csv")%>%
+p_mean <- read.csv("data/Timeseries.FBI.Payout.mean.csv")
+province_district <- read.csv("data/province district.csv")%>%
   rename(Province =1)
 
 forecast_mean=p_mean%>%
@@ -12,23 +10,40 @@ forecast_mean=p_mean%>%
   select(Year,Quantile,Province,Area,mean_observed_SPI6,mean_mean_Forecasted_SPI6_belowQ)
 head(forecast_mean)
 
+aggregate(mean_observed_SPI6 ~ Province, forecast_mean, mean)
 
-aa=aggregate(mean_observed_SPI6 ~ Province, forecast_mean, median)
 
-
-### Summary stats Mean SPI per province for each yes
+### Summary stats Mean SPI per province for each year
 library("dplyr")
 aa= forecast_mean %>%
-  filter(Year == 2019) %>%
+  filter(Year == 2021) %>%
   group_by(Province,Quantile ) %>%
   summarise(mean_observed_SPI6 = median(mean_observed_SPI6, na.rm = TRUE),mean_mean_Forecasted_SPI6_belowQ = median(mean_mean_Forecasted_SPI6_belowQ, na.rm = TRUE))
 
 ######### SPEI for each Plot Quantile for each district
+#  Exclude one area
+# library("ggplot2")
+# ggplot(subset(aa, Province != "Maputo"),
+#        aes(x = Quantile, y = mean_mean_Forecasted_SPI6_belowQ, color = Province)) +
+#   geom_line(lwd = 1, show.legend = FALSE) + facet_wrap(~ Province) +
+#   theme_bw() + theme(strip.text = element_text(size = rel(1.1)))
+
 library("ggplot2")
-ggplot(subset(aa, Province != "Manica"),
+ggplot(subset(aa, Province != "NA"),
        aes(x = Quantile, y = mean_mean_Forecasted_SPI6_belowQ, color = Province)) +
   geom_line(lwd = 1, show.legend = FALSE) + facet_wrap(~ Province) +
-  scale_color_manual(values = country_colors) +
-  theme_bw() + theme(strip.text = element_text(size = rel(1.1)))
+  theme_bw() + theme(strip.text = element_text(size = rel(1.1)))+
+  #theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
+lifeExp_plot <- ggplot(data = americas, mapping = aes(x = year, y = lifeExp, color=continent)) +
+  geom_line() + facet_wrap( ~ country) +
+  labs(
+    x = "Year",              # x axis title
+    y = "Life expectancy",   # y axis title
+    title = "Figure 1",      # main title of figure
+    color = "Continent"      # title of legend
+  ) +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+ggsave(filename = "results/lifeExp.png", plot = lifeExp_plot, width = 12, height = 10, dpi = 300, units = "cm")
 
